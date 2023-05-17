ff63f829c67cd5078b0e03b2a74081480b411b32
Alan pascke <alan.pascke@gmail.com>
2023-05-16 22:47:20 -0300

    algumas mudanças

--- routes/admin.js
+++ routes/admin.js
@@ -62,6 +62,65 @@ router.post('/customers/new', [
     }
 })
 
+router.get('/customers/edit/:id', async (req,res) =>{
+    try {
+        
+        const customer = await Customer.findByPk(req.params.id)
+        if (!customer) {
+            return res.status(400).send('cliente não encontrado');
+        }
+        res.render('admin/customers_add')
+    } catch (error) {
+        res.status(500).send('Erro ao buscar o cliente ' + error)
+    }
+})
+
+router.post('/customers/edit/:id', [
+    body('name').notEmpty().withMessage('O nome é obrigatório'),
+    body('email').notEmpty().withMessage('O email é obrigatório').isEmail().withMessage('Email inválido'),
+    body('cpf').notEmpty().withMessage('O CPF é obrigatório')
+  ], async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const customer = await Customer.findByPk(id);
+
+        !customer ? res.status(404).send('Cliente não encontrado') : true 
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            const errorMessages = errors.array().map(error => error.msg);
+            return res.render('admin/customers', { customer, errors: errorMessages });
+        }
+
+        await customer.update(req.body);
+
+        req.flash('success', 'Cliente atualizado com sucesso!');
+        res.redirect('/admin/customers');
+        } catch (error) {
+        res.status(500).send('Erro ao atualizar o cliente: ' + error);
+        }
+  });  
+
+
+function toggleButtons() {
+    const checkboxes = document.getElementsByName('selectedCustomers');
+
+    !checkboxes ? false : console.log('selecionado');
+    const editButtons = document.getElementsByClassName('btn-edit');
+    const deleteButtons = document.getElementsByClassName('btn-delete');
+  
+    const isAnyCheckboxSelected = Array.from(checkboxes).some(checkbox => checkbox.checked);
+  
+    Array.from(editButtons).forEach(button => {
+      button.style.display = isAnyCheckboxSelected ? 'inline-block' : 'none';
+    });
+  
+    Array.from(deleteButtons).forEach(button => {
+      button.style.display = isAnyCheckboxSelected ? 'inline-block' : 'none';
+    });
+}
+
 
 module.exports = router
 
